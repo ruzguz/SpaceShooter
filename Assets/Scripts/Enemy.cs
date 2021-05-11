f51bae2cdc596bf02d4354630a6c0f2283041fea
Rusben Guzman <rusbenguzman@gmail.com>
2021-05-11 17:46:42 -0400

    Created enemy prefab and added movement behaviour

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // General vars
+    [SerializeField] 
+    private float _speed = 4f;
+    private float _respawnYPosition = 8f;
+    private float _bottomLimit = -5.5f;
+    private float _horizontalLimit = 9.5f;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move enemy down at 4 meters per second
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        // If enemy goes out of the camera, respawn the enemy at the top i a new random x position
+        if (transform.position.y <= _bottomLimit) 
+        {
+            float newXposition = Random.Range(-_horizontalLimit, _horizontalLimit);
+            transform.position = new Vector3(newXposition, _respawnYPosition, 0);
+        }
+   
+    }
+}
