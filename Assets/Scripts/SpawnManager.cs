6866966abe02e1ccdbffa1e1d591625784e4259f
Rusben Guzman <rusbenguzman@gmail.com>
2021-05-13 18:18:41 -0400

    Created spawn enemy manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private float _startYPosition = 8f;
+    [SerializeField]
+    private float _xRange = 11f;
+    [SerializeField]
+    private float _spawnDelay = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        // Infinite loop
+        //    Instantiate an enemy prefab
+        //    wait 5 seconds
+        while(true)
+        {
+            // Calculating random position
+            float randomXPosition = Random.Range(-_xRange, _xRange);
+            Vector3 randomPosition = new Vector3(randomXPosition, _startYPosition, transform.position.z);
+            
+            // Spawning enemy and wait 5 seconds
+            Instantiate(_enemyPrefab, randomPosition, Quaternion.identity);
+            yield return new WaitForSeconds(_spawnDelay);
+        }
+    }
+}
