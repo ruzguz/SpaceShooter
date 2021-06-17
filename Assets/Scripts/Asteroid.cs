a590bb6924a04cb60cb721009ba0db8ae98be601
Rusben Guzman <rusbenguzman@gmail.com>
2021-06-17 18:43:42 -0400

    Create Asteroid

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _rotationSpeed = 4f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, Time.deltaTime * _rotationSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Laser")) 
+        {
+            Destroy(this.gameObject);
+        }    
+    }
+}
