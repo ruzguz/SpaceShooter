60904369015a9dbf6d03db38e1cd4af59bcb529c
Rusben Guzman <rusbenguzman@gmail.com>
2021-08-18 00:30:49 -0400

    FIX: restore enemy shield script

---
+++ Assets/Scripts/EnemyShield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShield : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player")) 
+        {
+            Player player = other.transform.GetComponent<Player>();
+
+            player.Damage();
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Laser")) 
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
